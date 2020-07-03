82d9d5d0d2cda6de46f8fb4c2edf65c73c515c29
Wubbb <baonicolas5@gmail.com>
2020-07-03 22:19:03 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let a = 1;
+let b = 2;
+//let c = b == 2 ? console.log('true') : console.log('false')
+
+switch(true){
+
+    case (b == 1): 
+        console.log('1')
+        break;
+    case (b == 2):
+        console.log('2')
+        break;
+    
+    default:
+        console.log('default')
+}
+
+let isShow = true;
+
+let cars = ["honda", "toyota", "mitsubishi", "chevy"]
+// console.log(cars.sort().reverse())
+
